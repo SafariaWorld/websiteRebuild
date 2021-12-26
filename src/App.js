4fd3e280eff9216ee338d53631ad1c92f482e10f
briandriscollcode <bdriscoll407@gmail.com>
2021-12-25 21:43:07 -0700

    starting rebuild

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from "./assets/Safaria_header_logo.png";
+import mainLogo from "./assets/Safaria.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <nav> 
+          <img src={logo} id="headerLogo" /> 
+
+          <ul class="headerText">
+              <li>
+                <a href="#"> Home </a>
+              </li>
+
+              <li>
+                <a href="#"> Roadmap </a>
+              </li>
+
+              <li>
+                <a href="#"> About </a>
+              </li>
+
+              <li>
+                <a href="#"> Updates </a>
+              </li>
+
+              <li>
+                <a href="#"> Collections </a>
+              </li>
+
+              <li>
+                <a href="#"> Lore </a>
+              </li>
+          </ul>
+            
+          <ul class="socialLinks">
+            <li> 
+              <a href="#"> Discord </a>
+            </li>
+            <li> 
+              <a href="#"> Twitter </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div class="mainHeader"> 
+          <img src={mainLogo} id="mainLogo" /> 
+          <h2> Find riches, explore ancient mysteries, and become a treasure
+            hunter in this CNFT Gamified World! </h2> 
+
+            <div id="headerButtons">
+              <button id="collectionButton"> Our Collections </button>
+              <button id="mintButton"> Mint a Treasure Hunter </button>
+            </div> 
+
+        </div>
+
       </header>
+
+      <body>
+        <main>
+          <h2> Content </h2> 
+        </main>
+      </body>
     </div>
   );
 }
